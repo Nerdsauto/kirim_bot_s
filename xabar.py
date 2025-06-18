4eed2ff76099cf37db3cb27306115e3334775d92
Nerdsauto <nerdsauto24@gmail.com>
2025-06-18 13:55:12 +0500

    Update xabar.py

--- xabar.py
+++ xabar.py
@@ -1,5 +1,1 @@
-TOKEN = "7976266533:AAH66Fal4sCsKwtlAmUiK5tzSGYMR6f86NY"
-CHAT_ID = -1002122162040  # Guruh yoki kanal chat id (manfiy son bilan)
-
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="Botdan test xabari!")
+{}
