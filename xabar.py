984994d039ba5755ec14b9b350e85f05641a9163
Nerdsauto <nerdsauto24@gmail.com>
2025-06-18 13:53:11 +0500

    Add files via upload

---
+++ xabar.py
@@ -0,0 +1,5 @@
+TOKEN = "7976266533:AAH66Fal4sCsKwtlAmUiK5tzSGYMR6f86NY"
+CHAT_ID = -1002122162040  # Guruh yoki kanal chat id (manfiy son bilan)
+
+bot = Bot(token=TOKEN)
+bot.send_message(chat_id=CHAT_ID, text="Botdan test xabari!")
